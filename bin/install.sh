d8df59c26634751157704d8720cb139e76108f7e
WDF-VIORuleZ <leon_gross@protonmail.ch>
2018-11-11 18:08:13 +0100

    Modified install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,26 +1,54 @@
 #!/bin/bash
 
-TITLE="Installing bash_scripts-git"
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+OK="[${GREEN}OK${NC}]"
+ERR="[${RED}ERR${NC}]"
 
 std_path="$HOME/.bin/"
-shell=$SHELL
+sh=echo $SHELL | cut -d'/' -f2-
 
-if [ ! -d "std_path" ];then
-  cd $HOME && mkdir .bin
-  # TODO: For other shells, detection
-  echo "Your stdshell is : $SHELL"
-  echo "Created $HOME/.bin/ ..."
+TITLE="Installing bash_scripts-git"
 
-  echo "Cloning repository ..."
-  git clone https://github.com/WDF-VIORuleZ/bash_scripts-git.git
+function write_to_rc(shell)
+{
+  shell_config = "~/."+$sh+"rc"
+  echo "# EXPORT VARIABLE BY " >> shell_config
+  echo 'export APP=/"$std_path"' >> shell_config
 
-  # Adding Direcotory ~/.bin to bashrc
-  echo "# EXPORT VARIABLE BY #{TODO INSERT NAME}" >> ~/.bashrc
-  echo 'export APP=/"$std_path"' >> ~/.bashrc
+  printf "${OK} Added export to $shell_config"
+}
 
-  echo "Added environment varibale in bashrc"
+function detect_shell(shell)
+{
+  valid = $(case $shell in
+  "bash") write_to_rc($shell) ;;
+  "zsh") write_to_rc($shell ;;
+  *) echo valid=false ;;
+  esac)
 
-  else
-    echo "[ERR] Directory $std_path already exists. Mission abort."
-    exit
-fi
+  return valid
+}
+
+main()
+{
+  if [ ! -d "std_path" ];then
+    cd $HOME && mkdir .bin
+    printf "${OK} Created Directory $HOME/.bin/"
+
+    echo "Your stdshell is : $SHELL"
+    if [ ! detect_shell($sh) ]
+      printf "${ERR} I'm sorry, but your shell is currently not supported
+      for adding the script directory to your path."
+    fi
+
+    echo "Cloning repository ..."std_path = "$HOME/.bin/"
+    git clone https://github.com/WDF-VIORuleZ/bash_scripts-git.git
+
+    else
+      echo "[ERR] Directory $std_path already exists. Mission abort."
+      exit
+  fi
+}
